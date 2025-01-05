4b2e97e3109d566ed6c07f2336f0a1226b955f28
Anubhav Raj <anubhavr219@gmail.com>
2025-01-05 13:16:44 +0530

    basic write sections and some minute changes in scripts

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 50; i++) {
     dot.style.left = `${Math.random() * 100}vw`;
     dot.style.top = `${Math.random() * 100}vh`;
     dot.style.animationDelay = `${Math.random() * 5}s`;
-    //document.body.appendChild(dot);
-    let main = document.getElementById('main')
+    document.body.appendChild(dot);
+    // let main = document.getElementById('main')
     main.appendChild(dot)
 }
