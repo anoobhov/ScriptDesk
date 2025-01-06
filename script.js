c762a74f10c35ec336286295309349b97cd86b12
Anubhav Raj <anubhavr219@gmail.com>
2025-01-06 12:23:52 +0530

    some changes

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-for (let i = 0; i < 50; i++) {
+// for (let i = 0; i < 50; i++) {
+//     const dot = document.createElement('div');
+//     dot.classList.add('dot');
+//     dot.style.left = `${Math.random() * 100}vw`;
+//     dot.style.top = `${Math.random() * 100}vh`;
+//     dot.style.animationDelay = `${Math.random() * 5}s`;
+//     document.body.appendChild(dot);
+//     // let main = document.getElementById('main')
+//     // main.appendChild(dot)
+// }
+
+// animation
+for (let i = 0; i < 70; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
-    dot.style.left = `${Math.random() * 100}vw`;
-    dot.style.top = `${Math.random() * 100}vh`;
+    const docWidth = document.documentElement.scrollWidth;
+    const docHeight = document.documentElement.scrollHeight;
+    
+    dot.style.left = `${Math.random() * docWidth}px`;
+    dot.style.top = `${Math.random() * docHeight}px`;
     dot.style.animationDelay = `${Math.random() * 5}s`;
     document.body.appendChild(dot);
-    // let main = document.getElementById('main')
-    main.appendChild(dot)
 }
+
+
+//handling the image inserted
+document.getElementById("imageInput").addEventListener("change", function (event) {
+    const file = event.target.files[0]; // Get the first selected file
+    const preview = document.getElementById("imagePreview");
+  
+    if (file) {
+      const reader = new FileReader(); // FileReader to read the file as a data URL
+      reader.onload = function (e) {
+        preview.innerHTML = `<img src="${e.target.result}" alt="Thumbnail">`; 
+      };
+      reader.readAsDataURL(file); // Read the file as a data URL
+    } else {
+      preview.innerHTML = "Thumbnail will appear here"; // Reset preview if no file
+    }
+  });
